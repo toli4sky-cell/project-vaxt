4d6e9eafe7626ea040c320fef4f0e3530bc15642
toli4sky-cell <toli4sky@gmail.com>
2026-03-30 10:45:01 +0500

    Delete backend directory

--- backend/app/repositories/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.repositories.user_repository import UserRepository
-
-__all__ = ["UserRepository"]
--- backend/app/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.services.auth_service import AuthService
-
-__all__ = ["AuthService"]
